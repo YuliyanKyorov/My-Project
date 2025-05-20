9937824f573eb71f71a81ee870e9c31c5263ce45
YuliyanKyorov <iskyorov5@gmail.com>
2025-05-20 13:42:25 +0300

    Add files via upload

---
+++ CreateMatrix.cs
@@ -0,0 +1,28 @@
+﻿Console.Write("Въведи брой редове: ");
+int rows = int.Parse(Console.ReadLine());
+
+Console.Write("Въведи брой колони: ");
+int cols = int.Parse(Console.ReadLine());
+
+int[,] matrix = new int[rows, cols];
+
+for (int i = 0; i < rows; i++)
+{
+    for (int j = 0; j < cols; j++)
+    {
+        Console.Write($"[{i},{j}]: ");
+        matrix[i, j] = int.Parse(Console.ReadLine());
+    }
+}
+
+Console.WriteLine("\nГотовата матрица е:");
+for (int i = 0; i < rows; i++)
+{
+    for (int j = 0; j < cols; j++)
+    {
+        Console.Write(matrix[i, j] + "\t");
+    }
+    Console.WriteLine();
+}                
+
+}
